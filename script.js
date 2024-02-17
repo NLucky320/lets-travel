adfdba989fa121c1bc4611aa498cdd7ac4c03203
Nlucky320 <luckyes144@gmail.com>
2024-02-17 11:39:40 +0600

    cost analysis done

--- script.js
+++ script.js
@@ -5,8 +5,45 @@ for (const btn of allBtn) {
     count = count + 1;
 
     const placeName = event.target.parentNode.childNodes[1].innerText;
+    const price = event.target.parentNode.childNodes[3].childNodes[1].innerText;
+    const placeContainer = document.getElementById("selected-place-container");
+    const li = document.createElement("li");
+    const place = document.createElement("P");
+    place.innerText = placeName;
+    const price2 = document.createElement("p");
+    price2.innerText = price;
+
+    li.appendChild(place);
+    li.appendChild(price2);
+
+    placeContainer.appendChild(li);
+
+    totalCost("total-cost", parseInt(price));
+    grandTotalCost("others");
     setInnerText("cart-count", count);
   });
+}
+
+function totalCost(elementId, value) {
+  const totalCost = document.getElementById(elementId).innerText;
+  const newTotalCost = parseInt(totalCost);
+  const sum = newTotalCost + parseInt(value);
+  setInnerText("total-cost", sum);
+}
+
+function grandTotalCost(category) {
+  const totalCost = document.getElementById("total-cost").innerText;
+  const newTotalCost = parseInt(totalCost);
+
+  if (category === "bus") {
+    setInnerText("grand-total", newTotalCost + 100);
+  } else if (category === "train") {
+    setInnerText("grand-total", newTotalCost - 200);
+  } else if (category === "flight") {
+    setInnerText("grand-total", newTotalCost + 500);
+  } else {
+    setInnerText("grand-total", newTotalCost);
+  }
 }
 
 function setInnerText(elementId, value) {
