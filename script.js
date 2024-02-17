f5c9834ec6963e8ee556e9c4cc198d0d6bed21a9
Nlucky320 <luckyes144@gmail.com>
2024-02-17 11:54:52 +0600

    button disabled done

--- script.js
+++ script.js
@@ -1,39 +1,46 @@
 const allBtn = document.getElementsByClassName("add-btn");
 let count = 0;
 for (const btn of allBtn) {
-  btn.addEventListener("click", function (event) {
-    count = count + 1;
+  btn.addEventListener(
+    "click",
+    function (event) {
+      count = count + 1;
 
-    const placeName = event.target.parentNode.childNodes[1].innerText;
-    const price = event.target.parentNode.childNodes[3].childNodes[1].innerText;
-    const placeContainer = document.getElementById("selected-place-container");
-    const li = document.createElement("li");
-    const place = document.createElement("P");
-    place.innerText = placeName;
-    const price2 = document.createElement("p");
-    price2.innerText = price;
+      const placeName = event.target.parentNode.childNodes[1].innerText;
+      const price =
+        event.target.parentNode.childNodes[3].childNodes[1].innerText;
+      const placeContainer = document.getElementById(
+        "selected-place-container"
+      );
+      const li = document.createElement("li");
+      const place = document.createElement("P");
+      place.innerText = placeName;
+      const price2 = document.createElement("p");
+      price2.innerText = price;
 
-    event.target.parentNode.parentNode.style.backgroundColor = "gray";
+      event.target.parentNode.parentNode.style.backgroundColor = "gray";
 
-    li.appendChild(place);
-    li.appendChild(price2);
+      li.appendChild(place);
+      li.appendChild(price2);
 
-    const budget = document.getElementById("budget").innerText;
-    const convertedBudget = parseInt(budget);
+      const budget = document.getElementById("budget").innerText;
+      const convertedBudget = parseInt(budget);
 
-    if (convertedBudget - parseInt(price) < 0) {
-      alert("low budget");
-      return;
-    }
-    document.getElementById("budget").innerText =
-      convertedBudget - parseInt(price);
+      if (convertedBudget - parseInt(price) < 0) {
+        alert("low budget");
+        return;
+      }
+      document.getElementById("budget").innerText =
+        convertedBudget - parseInt(price);
 
-    placeContainer.appendChild(li);
+      placeContainer.appendChild(li);
 
-    totalCost("total-cost", parseInt(price));
-    grandTotalCost("others");
-    setInnerText("cart-count", count);
-  });
+      totalCost("total-cost", parseInt(price));
+      grandTotalCost("others");
+      setInnerText("cart-count", count);
+    },
+    { once: true }
+  );
 }
 
 function totalCost(elementId, value) {
